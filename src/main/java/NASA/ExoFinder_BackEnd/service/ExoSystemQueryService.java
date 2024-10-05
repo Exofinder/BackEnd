fc5c94244752a754aeb0f213773d94df7631e13d
choyoungseo20 <whdudtj0809@naver.com>
2024-10-05 18:23:06 +0900

    Create habitability probability calculation API

--- src/main/java/NASA/ExoFinder_BackEnd/service/ExoSystemQueryService.java
+++ src/main/java/NASA/ExoFinder_BackEnd/service/ExoSystemQueryService.java
@@ -1,4 +1,10 @@
 package NASA.ExoFinder_BackEnd.service;
 
+import NASA.ExoFinder_BackEnd.domain.ExoSystem;
+
+import java.util.List;
+
 public interface ExoSystemQueryService {
+
+    List<ExoSystem> getPlanetInfoList(String hostName);
 }
--- src/main/java/NASA/ExoFinder_BackEnd/web/dto/ExoSystemResponseDTO.java
+++ src/main/java/NASA/ExoFinder_BackEnd/web/dto/ExoSystemResponseDTO.java
@@ -1,4 +1,18 @@
 package NASA.ExoFinder_BackEnd.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class ExoSystemResponseDTO {
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class HabitablePercentDTO {
+        private String planet;
+        private String habitablePercent;
+    }
 }
