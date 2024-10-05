049180a3510a685ee7fb5a3b99b3baf506cca2c1
choyoungseo20 <whdudtj0809@naver.com>
2024-10-05 14:04:08 +0900

    Set up

---
+++ src/main/java/NASA/ExoFinder_BackEnd/ExoFinderBackEndApplication.java
@@ -0,0 +1,13 @@
+package NASA.ExoFinder_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoFinderBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExoFinderBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NASA/ExoFinder_BackEnd/ExoFinderBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package NASA.ExoFinder_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoFinderBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
