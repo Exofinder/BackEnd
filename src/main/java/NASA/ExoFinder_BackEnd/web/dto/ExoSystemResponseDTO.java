4cd3289d10e64fbea4cbab0e3f03d873a16224e2
choyoungseo20 <whdudtj0809@naver.com>
2024-10-06 14:19:51 +0900

    Modification according to change of return parameter

--- src/main/java/NASA/ExoFinder_BackEnd/web/dto/ExoSystemResponseDTO.java
+++ src/main/java/NASA/ExoFinder_BackEnd/web/dto/ExoSystemResponseDTO.java
@@ -12,7 +12,12 @@ public class ExoSystemResponseDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class HabitablePercentDTO {
-        private String planet;
+        private String plName;
+        private String plOrbsmax;
+        private String stSpectype;
+        private String stBrightness;
+        private String innerBoundHabitableZone;
+        private String outerBoundHabitableZone;
         private String habitablePercent;
     }
 }
